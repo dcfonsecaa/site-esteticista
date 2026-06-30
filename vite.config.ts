f34cd1c059fdc5182fbe272e22d83c398bcf167b
Denis Fonseca <denis-fonseca@hotmail.com>
2026-06-30 10:33:07 -0300

    fix: add close plugin to prevent vite build hang

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+function ClosePlugin() {
+  return {
+    name: 'ClosePlugin',
+    closeBundle() {
+      console.log('Bundle closed')
+      process.exit(0)
+    },
+  }
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ClosePlugin()],
   base: '/',
 })
